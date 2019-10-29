bbb637c6ad4b1307b41e0d4ba2d1e8fd43716e30
tjmaynes <tjmaynes@gmail.com>
2019-10-29 13:30:28 -0400

    Add move html blog components to html.el

--- elisp/build-blog.el
+++ elisp/build-blog.el
@@ -7,6 +7,7 @@
 (require 'utilities)
 (require 'package-manager)
 (require 'seq)
+(require 'html)
 
 ;; Blog
 
@@ -46,145 +47,6 @@
 
 (defun blog/compile-latex-files (files)
   (seq-map 'blog/compile-latex-file files))
-
-(defun blog/get-head (title description)
-  (let ((blog-author-avatar-url (format "%s/%s" blog-url blog-author-avatar)))
-    (concat
-     "<head>\n"
-     (concat
-      "<meta charset=\"utf-8\">
-<title>" title "</title>
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
-<meta name=\"description\" content=\"" description "\" />
-<meta property=\"og:title\" content=\"" title "\" />
-<meta property=\"og:url\" content=\"https://tjmaynes.com/index.html\" />
-<meta property=\"og:description\" content=\"" description "\"/>
-<meta property=\"og:image\" content=\"" blog-author-avatar-url "\" />
-<meta property=\"og:type\" content=" "hello" " />
-<meta property=\"twitter:title\" content=\"" title "\" />
-<meta property=\"twitter:url\" content=\"https://tjmaynes.com/index.html\" />
-<meta property=\"twitter:image\" content=\"" blog-author-avatar-url "\" />
-<meta property=\"twitter:description\" content=\"" description "\" />
-<meta property=\"twitter:card\" content=\"" description "\" />
-<link rel=\"stylesheet\" type=\"text/css\" href=\"" blog-css-url "\">\n")
-     "</head>\n")))
-
-(defun blog/get-header ()
-  (concat
-   "<section class=\"content-header\">\n"
-   (concat
-    "<nav>
-     <ul>
-       <li><a href=\"/\"><img src=" blog-icon " alt=\"icon\" /></a></li>
-     </ul>
-     <ul>
-       <li><a href=\"/\">Home</a></li>
-       <li><a href=\"" blog-author-cv "\">CV</a></li>
-       <li><a href=\"/rss.xml\">Feed</a></li>
-     </ul>
-   </nav>\n")
-   "</section>\n"))
-
-(defun blog/get-footer ()
-  (concat
-   "<section class=\"content-footer\">\n"
-   (concat
-    "<article class=\"about\">
-     <img src=" blog-author-avatar ">
-     <p>" blog-author-description "</p>
-    </article>
-    <nav class=\"footer-menu\">
-     <ul>
-      <li><p><a href=\"https://github.com/" blog-author-github "\" target=\"_blank\">GitHub</a></p></li>
-      <li><p><a href=\"https://linkedin.com/in/" blog-author-linkedin "\" target=\"_blank\">LinkedIn</a></p></li>
-      <li><p><a href=\"https://twitter.com/" blog-author-twitter "\" target=\"_blank\">Twitter</a></p></li>
-      <li><p><a href=\"mailto:" blog-author-email "\">Contact</a></p></li>
-     </ul>
-     <ul>
-      <li>
-        <p><a href=\"https://github.com/tjmaynes/blog\">Built using Org-Mode ❤</a></p>
-      </li>
-     </ul>
-    </nav>")
-   "</section>\n"))
-
-(defun blog/get-body (content)
-  (concat
-   "<body>\n"
-   (concat
-    "<div class=\"content-wrapper\">\n"
-    (blog/get-header)
-    content
-    (blog/get-footer)
-    "</div>\n")
-   "</body>\n"))
-
-(defun blog/get-post-header (post-title post-date)
-  (let* ((xml-date-time (utilities/org-parse-and-format-date post-date "%F"))
-	 (display-date-time (utilities/org-parse-and-format-date post-date "%Y-%m-%d")))
-    (concat
-     "<header>\n"
-     (concat
-      "<h1 itemprop=\"name headline\">" post-title "</h1>\n"
-      "<p>Posted on <time datetime=\"" xml-date-time "\" itemprop=\"datePublished\">" display-date-time "</time> • " blog-author-name "</p>\n")
-   "</header>\n")))
-
-(defun blog/get-post-body (title date content)
-  (concat
-   "<div class=\"post\">\n"
-   (blog/get-post-header title date)
-   content
-   "</div>\n"))
-
-(defun blog/get-html (head body language)
-  (concat
-   "<!DOCTYPE html>\n"
-   (format "<html lang=\"%s\">\n" language)
-   head
-   body
-   "</html>\n"))
-
-(defun blog/base-html-template (title description language content)
-  (blog/get-html
-   (blog/get-head title description)
-   (blog/get-body content)
-   language))
-
-(defun blog/blog-index-template (content info)
-  (let* ((language (plist-get info :language)))
-    (blog/base-html-template blog-title
-			     blog-description
-			     language
-			     (concat
-			      "<div class=\"archive\">\n"
-			      content
-			      "</div>\n"))))
-
-(defun blog/blog-post-template (content info)
-  (let* ((title (utilities/org-get-file-keyword "TITLE"))
-	 (date (utilities/org-get-file-keyword "DATE"))
-	 (description (utilities/org-get-file-keyword "DESCRIPTION"))
-	 (language (plist-get info :language)))
-    (blog/base-html-template title
-			     description
-			     language
-			     (blog/get-post-body title date content))))
-
-(defun blog/get-page-body (title date content)
-  (concat
-   "<div class=\"post\">\n"
-   content
-   "</div>\n"))
-
-(defun blog/blog-page-template (content info)
-  (let* ((title (utilities/org-get-file-keyword "TITLE"))
-	 (date (utilities/org-get-file-keyword "DATE"))
-	 (description (utilities/org-get-file-keyword "DESCRIPTION"))
-	 (language (plist-get info :language)))
-    (blog/base-html-template title
-			     description
-			     language
-			     (blog/get-page-body title date content))))
 
 (defvar blog/yt-iframe-format
   (concat
@@ -337,11 +199,11 @@
   (require 'ox)
   (blog/org-add-link-types)
   (org-export-define-derived-backend 'custom-blog-index-backend 'html
-				     :translate-alist '((template . blog/blog-index-template)))
+				     :translate-alist '((template . html/index-page-template)))
   (org-export-define-derived-backend 'custom-blog-post-backend 'html
-				     :translate-alist '((template . blog/blog-post-template)))
+				     :translate-alist '((template . html/post-page-template)))
   (org-export-define-derived-backend 'custom-blog-page-backend 'html
-				     :translate-alist '((template . blog/blog-page-template))))
+				     :translate-alist '((template . html/static-page-template))))
 
 (defun blog/publish-all ()
   (package-manager/ensure-packages-installed 'org 'org-plus-contrib 'htmlize 'org-re-reveal)
---
+++ elisp/html.el
@@ -0,0 +1,140 @@
+(provide 'html)
+
+(defun html/get-head (title description)
+  (let ((blog-author-avatar-url (format "%s/%s" blog-url blog-author-avatar)))
+    (concat
+     "<head>\n"
+     (concat
+      "<meta charset=\"utf-8\">
+<title>" title "</title>
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+<meta name=\"description\" content=\"" description "\" />
+<meta property=\"og:title\" content=\"" title "\" />
+<meta property=\"og:url\" content=\"https://tjmaynes.com/index.html\" />
+<meta property=\"og:description\" content=\"" description "\"/>
+<meta property=\"og:image\" content=\"" blog-author-avatar-url "\" />
+<meta property=\"og:type\" content=" "hello" " />
+<meta property=\"twitter:title\" content=\"" title "\" />
+<meta property=\"twitter:url\" content=\"https://tjmaynes.com/index.html\" />
+<meta property=\"twitter:image\" content=\"" blog-author-avatar-url "\" />
+<meta property=\"twitter:description\" content=\"" description "\" />
+<meta property=\"twitter:card\" content=\"" description "\" />
+<link rel=\"stylesheet\" type=\"text/css\" href=\"" blog-css-url "\">\n")
+     "</head>\n")))
+
+(defun html/get-header ()
+  (concat
+   "<section class=\"content-header\">\n"
+   (concat
+    "<nav>
+     <ul>
+       <li><a href=\"/\"><img src=" blog-icon " alt=\"icon\" /></a></li>
+     </ul>
+     <ul>
+       <li><a href=\"/\">Home</a></li>
+       <li><a href=\"" blog-author-cv "\">CV</a></li>
+       <li><a href=\"/rss.xml\">Feed</a></li>
+     </ul>
+   </nav>\n")
+   "</section>\n"))
+
+(defun html/get-footer ()
+  (concat
+   "<section class=\"content-footer\">\n"
+   (concat
+    "<article class=\"about\">
+     <img src=" blog-author-avatar ">
+     <p>" blog-author-description "</p>
+    </article>
+    <nav class=\"footer-menu\">
+     <ul>
+      <li><p><a href=\"https://github.com/" blog-author-github "\" target=\"_blank\">GitHub</a></p></li>
+      <li><p><a href=\"https://linkedin.com/in/" blog-author-linkedin "\" target=\"_blank\">LinkedIn</a></p></li>
+      <li><p><a href=\"https://twitter.com/" blog-author-twitter "\" target=\"_blank\">Twitter</a></p></li>
+      <li><p><a href=\"mailto:" blog-author-email "\">Contact</a></p></li>
+     </ul>
+     <ul>
+      <li>
+        <p><a href=\"https://github.com/tjmaynes/blog\">Built using Org-Mode ❤️</a></p>
+      </li>
+     </ul>
+    </nav>")
+   "</section>\n"))
+
+(defun html/get-body (content)
+  (concat
+   "<body>\n"
+   (concat
+    "<div class=\"content-wrapper\">\n"
+    (html/get-header)
+    content
+    (html/get-footer)
+    "</div>\n")
+   "</body>\n"))
+
+(defun html/get-post-header (post-title post-date)
+  (let* ((xml-date-time (utilities/org-parse-and-format-date post-date "%F"))
+	 (display-date-time (utilities/org-parse-and-format-date post-date "%Y-%m-%d")))
+    (concat
+     "<header>\n"
+     (concat
+      "<h1 itemprop=\"name headline\">" post-title "</h1>\n"
+      "<p>Posted on <time datetime=\"" xml-date-time "\" itemprop=\"datePublished\">" display-date-time "</time> • " blog-author-name "</p>\n")
+   "</header>\n")))
+
+(defun html/get-post-page-body (title date content)
+  (concat
+   "<div class=\"post\">\n"
+   (html/get-post-header title date)
+   content
+   "</div>\n"))
+
+(defun html/get-html (head body language)
+  (concat
+   "<!DOCTYPE html>\n"
+   (format "<html lang=\"%s\">\n" language)
+   head
+   body
+   "</html>\n"))
+
+(defun html/get-static-page-body (title date content)
+  (concat
+   "<div class=\"post\">\n"
+   content
+   "</div>\n"))
+
+(defun html/base-html-template (title description language content)
+  (html/get-html
+   (html/get-head title description)
+   (html/get-body content)
+   language))
+
+(defun html/index-page-template (content info)
+  (let* ((language (plist-get info :language)))
+    (html/base-html-template blog-title
+			     blog-description
+			     language
+			     (concat
+			      "<div class=\"archive\">\n"
+			      content
+			      "</div>\n"))))
+
+(defun html/post-page-template (content info)
+  (let* ((title (utilities/org-get-file-keyword "TITLE"))
+	 (date (utilities/org-get-file-keyword "DATE"))
+	 (description (utilities/org-get-file-keyword "DESCRIPTION"))
+	 (language (plist-get info :language)))
+    (html/base-html-template title
+			     description
+			     language
+			     (html/get-post-page-body title date content))))
+
+(defun html/static-page-template (content info)
+  (let* ((title (utilities/org-get-file-keyword "TITLE"))
+	 (date (utilities/org-get-file-keyword "DATE"))
+	 (description (utilities/org-get-file-keyword "DESCRIPTION"))
+	 (language (plist-get info :language)))
+    (html/base-html-template title
+			     description
+			     language
+			     (html/get-static-page-body title date content))))
